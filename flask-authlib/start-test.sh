61427aa6278de8d4829c216866162e4116db8d19
Farhan Patwa <farhan.patwa@starlab.io>
2018-03-29 10:39:13 -0400

    Updates to handle setting of the SQLite DB location correctly
    This script assumes that the file flask-authlib/conf/dev.config.py
    has the followiing value of the database URI
    SQLALCHEMY_DATABASE_URI = 'sqlite:////<USER_HOME_DIR>/<GALAHAD_HOME_DIR>/flask-authlib/website/sqlite.db'
    
    This will be updated to the correct directory structure based on the current directory path from
    which this script will be run.
    This will help to be able to clone the repo and run the flask test server in any directory
    with no manual changes.

--- flask-authlib/start-test.sh
+++ flask-authlib/start-test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Get the current directory
+current_dir=$(pwd)
+# Now get the last field in the directory structure - This should be
+# the parent directory from where this script is being called.
+dir=$(echo ${current_dir##*/})
+
 # Ensure that this script is being run from the correct directory
-if [ ! -f app.py ]; then
-  echo ""
+if [[ $dir != "flask-authlib" ]]; then
   echo "#########################################################################################"
   echo "ERROR: Incorrect directory detected!!"
-  echo "ERROR: Unable to find file app.py in current directory"
+  echo "ERROR: The current parent directory is not [flask-authlib]"
   echo "ERROR: Please ensure this script is being run from \$HOME/galahad/flask-authlib directory"
   echo "#########################################################################################"
 else
@@ -20,6 +25,11 @@ else
     # These are needed to initialize flask's database
     export FLASK_APP=app.py
     export FLASK_DEBUG=1
+    # Update the SQLite DB file location in the config
+    # Get the directory path without the 'flask-authlib' dir
+    directory_path=$(sed 's/\/flask-authlib//' <<< $current_dir)
+    # Now replace the placeholders with the correct directory location
+    $(sed -i 's,/<USER_HOME_DIR>\/<GALAHAD_HOME_DIR>,'"$directory_path"',' $current_dir/conf/dev.config.py)
     # Initialize the database.
     flask initdb
   fi
