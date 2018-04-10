2e083592bb9a8ceccc5732790c15e5acba4501e7
sponomar <sponomar@bbn.com>
2018-04-10 15:31:52 -0400

    Updated assembler to handle apparmor properly

--- assembler/stages/kernel.py
+++ assembler/stages/kernel.py
@@ -1,5 +1,5 @@
 from stages.core.ssh_stage import SSHStage
-
+import time
 import subprocess, os
 
 class KernelStage(SSHStage):
@@ -15,3 +15,13 @@ class KernelStage(SSHStage):
             for f in files:
                 self._copy_file(os.path.join(deb_file_path, f), f)
             self._exec_cmd('sudo dpkg -i --force-all '+' '.join(files))
+            for f in files:
+                self._exec_cmd('rm %s' % (f))
+            try:
+                self._exec_cmd('sudo reboot')
+            except subprocess.CalledProcessError as e:
+                if e.returncode == 255: # reboot will kill the connection causing 255 error code
+                    pass
+                else:
+                    raise
+            time.sleep(10) # wait 10 seconds to reboot
