b65e9a1453c2933e76ec721f74737e1af2b7e726
Jeffrey Mitchell <jeffrey.mitchell@starlab.io>
2018-05-22 09:00:25 -0500

    Split test.py into three files setup_aws_inst() will use git pull to put the galahad repo onto the aws instance, rather than scp

--- tests/virtue-ci/test.py
+++ tests/virtue-ci/test.py
@@ -1,81 +1,12 @@
 #!/usr/bin/python
 
-import os
-import sys
-import time
-import subprocess
-import boto3
 import argparse
 
+if( __name__ == '__main__' ):
+    import setup
+    from test_common import ssh_tool
+
 AMI_ID = 'ami-aa2ea6d0' # Default AMI
-
-def ssh( dns, command, test=True ):
-
-    print
-    print
-    print "{0}  {1}".format( dns, command )
-    print ' '.join( ['ssh', '-i', args.sshkey, args.username + '@' + dns, command] )
-    print
-
-    ret = subprocess.call( ['ssh', '-i', args.sshkey, '-o', 'StrictHostKeyChecking=no', args.username + '@' + dns, command] )
-
-    # By default, it is not ok to fail
-    if( test ):
-        assert ret == 0
-
-    return ret
-
-def scp_to( dns, file_path_local, file_path_remote='', test=True ):
-
-    print
-    print
-    print "{0}  {1}  {2}".format( dns, file_path_local, file_path_remote )
-    print ' '.join( ['scp', '-r', '-i', args.sshkey, file_path_local, args.username + '@' + dns + ':' + file_path_remote] )
-    print
-
-    ret = subprocess.call( ['scp', '-r', '-i', args.sshkey, file_path_local, args.username + '@' + dns + ':' + file_path_remote] )
-
-    # By default, it is not ok to fail
-    if( test ):
-        assert ret == 0
-
-    return ret
-
-def setup_aws_inst( inst_ip ):
-
-    # Install required packages
-    ssh( inst_ip, 'sudo apt-get update' )
-    #ssh( inst_ip, 'sudo apt-get upgrade -y' )
-    ssh( inst_ip, 'sudo apt-get install -y python-pip libldap2-dev libsasl2-dev unzip' )
-
-    # scp galahad/
-    dirs = os.listdir( os.environ['VIRTUE_ROOT'] )
-
-    if( '.git' in dirs ):
-        dirs.remove( '.git' )
-
-    ssh( inst_ip, 'mkdir ~/galahad', test=False )
-    for x in dirs:
-        scp_to( inst_ip, '{0}/{1}'.format( os.environ['VIRTUE_ROOT'], x ), '~/galahad' )
-
-    # Install and configure slapd
-    ssh( inst_ip, 'sudo ~/galahad/tests/virtue-ci/install_ldap.sh' )
-    ssh( inst_ip, 'echo \'export LDAPSEARCH="ldapsearch -H ldap://localhost -D cn=admin,dc=canvas,dc=virtue,dc=com -W -b dc=canvas,dc=virtue,dc=com"\' >> ~/.bashrc' )
-
-    # Install required python packages
-    ssh( inst_ip, 'sudo pip install --upgrade -r ~/galahad/flask-authlib/requirements.txt' )
-
-    # Setup the schema
-    ssh( inst_ip, '~/galahad/tests/virtue-ci/add_canvas_schema.sh' )
-
-    # Create base LDAP structure
-    ssh( inst_ip, '~/galahad/tests/virtue-ci/create_base_ldap_structure.py' )
-
-    # Setup AWS client on AWS inst
-    ssh( inst_ip, 'sudo apt-get install -y awscli' )
-    ssh( inst_ip, 'mkdir .aws', test=False )
-    scp_to( inst_ip, os.environ['VIRTUE_ROOT'] + '/tests/virtue-ci/aws_config', '~/.aws/config' )
-    scp_to( inst_ip, args.awskeys, '~/.aws/credentials' )
 
 def parse_args():
 
@@ -85,6 +16,9 @@ def parse_args():
                          help='The path to the private key to use ssh with' )
     parser.add_argument( '-k', '--awskeys', type=str, required=True,
                          help='Path to the AWS credentials for the ec2 instance to use' )
+    parser.add_argument( '-g', '--github_key', type=str, required=True,
+                         help='Path to a private ssh key to clone the github ' +
+                         'repo on the ec2 instance' )
     parser.add_argument( '-m', '--iamname', type=str, required=False, default='',
                          help='The AWS IAM name to use while creating the ec2 instance' )
     parser.add_argument( '-I', '--ip', type=str, required=False,
@@ -109,70 +43,24 @@ if( __name__ == '__main__' ):
     args = parse_args()
 
     if( args.ip == None ):
-
-        if( args.subnet == None ):
-            print( 'Need subnet value -s' )
-            exit()
-
-        # Get the name of the key file without the .pem file extension
         key_name = '.'.join( args.sshkey.split('/')[-1].split('.')[0:-1] )
 
-        # Create AWS inst
-        ec2 = boto3.resource('ec2', region_name='us-east-1')
-
-        res = ec2.create_instances( ImageId=args.ami,
-                                    InstanceType='t2.micro',
-                                    KeyName=key_name,
-                                    MinCount=1,
-                                    MaxCount=1,
-                                    Monitoring={'Enabled': False},
-                                    SubnetId=args.subnet,
-                                    IamInstanceProfile={
-                                        'Name': args.iamname
-                                    },
-                                    TagSpecifications=[
-                                        {
-                                            'ResourceType': 'instance',
-                                            'Tags': [{
-                                                'Key': 'Project',
-                                                'Value': 'Virtue'
-                                            },
-                                            {
-                                                'Key': 'Name',
-                                                'Value': args.name
-                                            }]
-                                        },
-                                        {
-                                            'ResourceType': 'volume',
-                                            'Tags': [{
-                                                'Key': 'Project',
-                                                'Value': 'Virtue'
-                                            }]
-                                        }
-                                    ] )
-
-        instance = res[0]
-        instance.wait_until_running()
-        time.sleep(5)
-        instance.reload()
-
-        print( instance.id )
-
-        aws_ip = instance.public_ip_address
-
+        aws_ip = setup.create_aws_inst( args.ami, args.subnet, args.iamname, args.name, key_name )
     else:
-
         aws_ip = args.ip
 
     print( aws_ip )
+
+    ssh_inst = ssh_tool( args.username, aws_ip )
+    ssh_inst.sshkey = args.sshkey
     
     # Setup the VM
-    setup_aws_inst( aws_ip )
+    setup.setup_aws_inst( ssh_inst, args.github_key, args.awskeys )
 
     # Run individual tests
-    ssh( aws_ip, 'cd galahad/tests/virtue-ci && ./run_ldap_tests.py' )
-    ssh( aws_ip, 'cd galahad/tests/virtue-ci && ./run_admin_api_tests.py' )
-    ssh( aws_ip, 'cd galahad/tests/virtue-ci && ./run_user_api_tests.py' )
+    ssh_inst.ssh( 'cd galahad/tests/virtue-ci && ./run_ldap_tests.py' )
+    ssh_inst.ssh( 'cd galahad/tests/virtue-ci && ./run_admin_api_tests.py' )
+    ssh_inst.ssh( 'cd galahad/tests/virtue-ci && ./run_user_api_tests.py' )
 
     print
     print( 'Success' )
