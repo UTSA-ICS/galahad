bcb54ffa73005e7005b58edc9bb27ec4ffce3f08
Jonathan Price <jonathan.price@raytheon.com>
2018-05-31 11:29:13 -0400

    Update syslog-ng node config with sshd logging and filtering * Updated Assembler script with same

--- assembler/stages/transducer_install.py
+++ assembler/stages/transducer_install.py
@@ -94,16 +94,125 @@ parser transducer_controller {
 	transducer_controller();
 };
 
-log { 
-	source(s_local); 
-	filter { match("kernel" value("PROGRAM")) or match("winesrv" value("PROGRAM")) };
-    parser(message_parser);
-	parser(transducer_controller);
-    filter { not match("syslog-ng" value("ProcName")) };
-	destination(d_file);
-	destination(d_elastic);
-	destination(d_network);
+filter remove_self_reports {
+        not (match("syslog-ng" value("ProcName")) or
+        match("syslog-ng" value("ParentName")));
 };
+
+filter remove_merlin_reports {
+        not match("/var/run/receiver_to_filter" value("Addr"));
+};
+
+log { 
+    source(s_local); 
+    filter { match("kernel" value("PROGRAM")) or match("winesrv" value("PROGRAM")) };
+    parser(message_parser);
+    parser(transducer_controller);
+    filter(remove_self_reports);
+    filter(remove_merlin_reports);
+    destination(d_file);
+    destination(d_elastic);
+    destination(d_network);
+};
+
+
+log {
+    source(s_local);
+    filter{ facility(auth); };
+    destination(d_file);
+    destination(d_elastic);
+    destination(d_network);
+};
+    '''
+
+    SSHD_CONFIG = '''# Package generated configuration file
+# See the sshd_config(5) manpage for details
+
+# What ports, IPs and protocols we listen for
+Port 22
+# Use these options to restrict which interfaces/protocols sshd will bind to
+#ListenAddress ::
+#ListenAddress 0.0.0.0
+Protocol 2
+# HostKeys for protocol version 2
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_dsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+HostKey /etc/ssh/ssh_host_ed25519_key
+#Privilege Separation is turned on for security
+UsePrivilegeSeparation yes
+
+# Lifetime and size of ephemeral version 1 server key
+KeyRegenerationInterval 3600
+ServerKeyBits 1024
+
+# Logging
+SyslogFacility AUTH
+LogLevel VERBOSE
+
+# Authentication:
+LoginGraceTime 120
+PermitRootLogin prohibit-password
+StrictModes yes
+
+RSAAuthentication yes
+PubkeyAuthentication yes
+#AuthorizedKeysFile     %h/.ssh/authorized_keys
+
+# Don't read the user's ~/.rhosts and ~/.shosts files
+IgnoreRhosts yes
+# For this to work you will also need host keys in /etc/ssh_known_hosts
+RhostsRSAAuthentication no
+# similar for protocol version 2
+HostbasedAuthentication no
+# Uncomment if you don't trust ~/.ssh/known_hosts for RhostsRSAAuthentication
+#IgnoreUserKnownHosts yes
+
+# To enable empty passwords, change to yes (NOT RECOMMENDED)
+PermitEmptyPasswords no
+
+# Change to yes to enable challenge-response passwords (beware issues with
+# some PAM modules and threads)
+ChallengeResponseAuthentication no
+
+# Change to no to disable tunnelled clear text passwords
+PasswordAuthentication no
+
+# Kerberos options
+#KerberosAuthentication no
+#KerberosGetAFSToken no
+#KerberosOrLocalPasswd yes
+#KerberosTicketCleanup yes
+
+# GSSAPI options
+#GSSAPIAuthentication no
+#GSSAPICleanupCredentials yes
+
+X11Forwarding yes
+X11DisplayOffset 10
+PrintMotd no
+PrintLastLog yes
+TCPKeepAlive yes
+#UseLogin no
+
+#MaxStartups 10:30:60
+#Banner /etc/issue.net
+
+# Allow client to pass locale environment variables
+AcceptEnv LANG LC_*
+
+Subsystem sftp /usr/lib/openssh/sftp-server
+
+# Set this to 'yes' to enable PAM authentication, account processing,
+# and session processing. If this is enabled, PAM authentication will
+# be allowed through the ChallengeResponseAuthentication and
+# PasswordAuthentication.  Depending on your PAM configuration,
+# PAM authentication via ChallengeResponseAuthentication may bypass
+# the setting of "PermitRootLogin without-password".
+# If you just want the PAM account and session checks to run without
+# PAM authentication, then enable this but set PasswordAuthentication
+# and ChallengeResponseAuthentication to 'no'.
+UsePAM yes
     '''
 
     ELASTIC_YML = '''cluster:
@@ -125,6 +234,7 @@ searchguard.ssl.transport.enforce_hostname_verification: false
             kirk_filename = 'kirk-keystore.jks'
             truststore_filename = 'truststore.jks'
             install_script_filename = 'runme.sh'
+            sshd_config_path = '/etc/ssh/sshd_config'
 
 
             module_path = os.path.join(self.PAYLOAD_PATH, self.MODULE_TARBALL)
