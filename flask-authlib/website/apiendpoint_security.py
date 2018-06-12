8b8df2b9b58adfb8782c9fcfa70de2d7d358a97b
Maria Zakhalyavko <mariya.zakhalyavko@raytheon.com>
2018-06-12 18:18:56 +0000

    Fix user authentication for security API and some log messages
    References issue #80

--- flask-authlib/website/apiendpoint_security.py
+++ flask-authlib/website/apiendpoint_security.py
@@ -27,7 +27,7 @@ class EndPoint_Security:
             self.__class__.conn = None
 
     # Not officially part of the API but necessary to set up paths correctly
-    def set_api_config(self, userToken, config):
+    def set_api_config(self, config):
         # Default values
         self.__class__.rethinkdb_host = 'rethinkdb.galahad.lab'
         self.__class__.ca_cert = 'rethinkdb_cert.pem'
@@ -59,14 +59,11 @@ class EndPoint_Security:
         if not os.path.isdir(self.__class__.virtue_key_dir):
             return self.__error('invalidOrMissingParameters', details='Directory not found for Virtue public keys: ' + self.__class__.virtue_key_dir)
 
-    ## TODO: need a function that will confirm that the userToken is valid
-
-    def transducer_list(self, userToken):
+    def transducer_list(self):
         '''
         Lists all transducers currently available in the system
 
         Args:
-            userToken (UserToken): The UserToken for the logged-in admin user
 
         Returns:
             list: Transducer objects for known transducers
@@ -75,7 +72,7 @@ class EndPoint_Security:
         try:
             transducers_raw = self.inst.get_objs_of_type('OpenLDAPtransducer')
             if transducers_raw is None:
-                return json.dumps(ErrorCodes.user['unspecifedError'])
+                return self.__error('unspecifiedError', details='Unable to get transducer objects from LDAP')
 
             transducers_ret = []
 
@@ -86,16 +83,14 @@ class EndPoint_Security:
             return json.dumps(transducers_ret)
 
         except Exception as e:
-            print( "Error: {0}".format(e) )
-            return json.dumps( ErrorCodes.user['unspecifiedError'] )
+            return self.__error('unspecifiedError', details='Unable to get transducer objects: ' + str(e))
 
-    def transducer_get(self, userToken, transducerId):
+    def transducer_get(self, transducerId):
         '''
         Gets information about the indicated Transducer. Does not include information about any
         instantiation in Virtues.
 
         Args:
-            userToken (UserToken): The UserToken for the logged-in admin user
             transducerId (str): The ID of the Transducer to get.
 
         Returns:
@@ -105,7 +100,7 @@ class EndPoint_Security:
         try:
             transducer = self.inst.get_obj( 'cid', transducerId, 'openLDAPtransducer', True )
             if transducer is None or transducer == ():
-                return json.dumps( ErrorCodes.user['invalidId'] )
+                return self.__error('invalidTransducerId')
             ldap_tools.parse_ldap(transducer)
 
             if DEBUG_PERMISSIONS:
@@ -114,15 +109,13 @@ class EndPoint_Security:
             return json.dumps(transducer)
 
         except Exception as e:
-            print( "Error: {0}".format(e) )
-            return json.dumps( ErrorCodes.user['unspecifiedError'] )
+            return self.__error('unspecifiedError', details='Unable to get transducer object: ' + str(e))
 
-    def transducer_enable(self, userToken, transducerId, virtueId, configuration):
+    def transducer_enable(self, transducerId, virtueId, configuration):
         '''
         Enables the indicated Transducer in the indicated Virtue.
 
         Args:
-            userToken (UserToken) : The UserToken for the logged-in admin user
             transducerId (str) : The ID of the Transducer to enable. 
             virtueId (str)     : The ID of the Virtue in which to enable the Transducer. 
             configuration (object): The configuration to apply to the Transducer when it is enabled. Format 
@@ -132,28 +125,26 @@ class EndPoint_Security:
             bool: True if the transducer was enabled, false otherwise
 
         '''
-        return self.__enable_disable(userToken, transducerId, virtueId, configuration, True)
+        return self.__enable_disable(transducerId, virtueId, configuration, True)
 
-    def transducer_disable(self, userToken, transducerId, virtueId):
+    def transducer_disable(self, transducerId, virtueId):
         '''
         Disables the indicated Transducer in the indicated Virtue
 
         Args:
-            userToken (UserToken) : The UserToken for the logged-in admin user
             transducerId (str) : The ID of the Transducer to disable
             virtueId (str)     : The ID of the Virtue in which to enable the Transducer. 
 
         Returns:
             bool: True if the transducer was enabled, false otherwise
         '''
-        return self.__enable_disable(userToken, transducerId, virtueId, None, False)
+        return self.__enable_disable(transducerId, virtueId, None, False)
 
-    def transducer_get_enabled(self, userToken, transducerId, virtueId):
+    def transducer_get_enabled(self, transducerId, virtueId):
         '''
         Gets the current enabled status for the indicated Transducer in the indicated Virtue.
 
         Args:
-            userToken (UserToken) : The UserToken for the logged-in admin user
             transducerId (str) : The ID of the Transducer
             virtueId (str)     : The ID of the Virtue in which to enable the Transducer. 
 
@@ -168,17 +159,16 @@ class EndPoint_Security:
             row = r.db('transducers').table('acks')\
                 .get([virtueId, transducerId]).run(self.__class__.conn)
         except r.ReqlError as e:
-            return self.__error('unspecifiedError', details='Failed to get info about transducer')
+            return self.__error('unspecifiedError', details='Failed to get info about transducer: ' + str(e))
 
         self.__verify_message(row)
         return row['enabled']
 
-    def transducer_get_configuration(self, userToken, transducerId, virtueId):
+    def transducer_get_configuration(self, transducerId, virtueId):
         '''
         Gets the current configuration for the indicated Transducer in the indicated Virtue.
 
         Args:
-            userToken (UserToken) : The UserToken for the logged-in admin user
             transducerId (str) : The ID of the Transducer
             virtueId (str)     : The ID of the Virtue in which to enable the Transducer. 
 
@@ -192,17 +182,16 @@ class EndPoint_Security:
             row = r.db('transducers').table('acks')\
                 .get([virtueId, transducerId]).run(self.__class__.conn)
         except r.ReqlError as e:
-            return self.__error('unspecifiedError', details='Failed to get info about transducer')
+            return self.__error('unspecifiedError', details='Failed to get info about transducer: ' + str(e))
 
         self.__verify_message(row)
         return row['configuration']
 
-    def transducer_list_enabled(self, userToken, virtueId):
+    def transducer_list_enabled(self, virtueId):
         '''
         Lists all Transducers currently that are currently enabled in the indicated Virtue.
 
         Args:
-            userToken (UserToken) : The UserToken for the logged-in admin user
             virtueId (str)     : The ID of the Virtue in which to enable the Transducer. 
 
         Returns:
@@ -219,11 +208,10 @@ class EndPoint_Security:
 
                 self.__verify_message(row)
                 if ('enabled' in row) and row['enabled']:
-                    print row['transducer_id']
                     enabled_transducers.append(row['transducer_id'])
 
         except r.ReqlError as e:
-            return self.__error('unspecifiedError', details='Failed to get enabled transducers')
+            return self.__error('unspecifiedError', details='Failed to get enabled transducers: ' + str(e))
 
         return enabled_transducers
 
@@ -242,19 +230,19 @@ class EndPoint_Security:
             except r.ReqlDriverError as e:
                 return self.__error('unspecifiedError', details=\
                     'Failed to connect to RethinkDB at host: ' + \
-                    self.__class__.rethinkdb_host)
+                    self.__class__.rethinkdb_host + ' because: ' + str(e))
 
-    def __enable_disable(self, userToken, transducerId, virtueId, configuration, isEnable):
+    def __enable_disable(self, transducerId, virtueId, configuration, isEnable):
 
         # Make sure transducer exists
         transducer = self.inst.get_obj( 'cid', transducerId, 'openLDAPtransducer', True )
         if transducer is None or transducer == ():
-            return json.dumps( ErrorCodes.user['invalidId'] )
+            return self.__error('invalidTransducerId')
         
         # Make sure virtue exists
         virtue = self.inst.get_obj( 'cid', virtueId, 'openLDAPvirtue', True )
         if virtue is None or virtue == ():
-            return json.dumps( ErrorCodes.user['invalidId'] )
+            return self.__error('invalidVirtueId')
 
         # Change the ruleset
         ret = self.__change_ruleset(virtueId, transducerId, isEnable, config=configuration)
@@ -262,12 +250,11 @@ class EndPoint_Security:
             return ret
 
         # Update the virtue's list of transducers
-        new_t_list = self.transducer_list_enabled(userToken, virtueId)
+        new_t_list = self.transducer_list_enabled(virtueId)
         virtue['ctransIds'] = str(new_t_list)
         ret = self.inst.modify_obj('cid', virtueId, virtue, 'OpenLDAPvirtue', True)
         if ret != 0:
-            # TODO fix error type
-            return json.dumps( ErrorCodes.user['resourceCreationError'] )
+            return self.__error('unspecifiedError', details='Unable to update virtue\'s list of transducers')
 
         return True
 
@@ -341,7 +328,7 @@ class EndPoint_Security:
             if res['errors'] > 0:
                 return self.__error('unspecifiedError', details='Failed to insert into commands table; first error: ' + res['first_error'])
         except r.ReqlError as e:
-            return self.__error('unspecifiedError', details='Failed to insert into commands table')
+            return self.__error('unspecifiedError', details='Failed to insert into commands table: ' + str(e))
 
         # Wait for ACK from the virtue that the ruleset has been changed
         #try:
