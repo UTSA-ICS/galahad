5b624750f9bc29cec1deca951d13653fc48fe621
Jonathan Price <jonathan.price@raytheon.com>
2018-06-12 14:11:46 -0400

    Modify transducer_install.py to place sshd_config in the correct place

--- assembler/stages/transducer_install.py
+++ assembler/stages/transducer_install.py
@@ -48,6 +48,7 @@ apt update && apt install syslog-ng-core curl git -y
         mv elasticsearch.yml /etc/syslog-ng/
         mv kirk-keystore.jks /etc/syslog-ng/
         mv truststore.jks /etc/syslog-ng/
+        mv sshd_config /etc/ssh/
         systemctl enable syslog-ng
         systemctl start syslog-ng
 
@@ -145,15 +146,16 @@ searchguard.ssl.transport.enforce_hostname_verification: false
             truststore_filename = 'truststore.jks'
             install_script_filename = 'runme.sh'
             sshd_config_filename = 'sshd_config'
-            sshd_config_path = '/etc/ssh/sshd_config'
 
 
             module_path = os.path.join(self.PAYLOAD_PATH, self.MODULE_TARBALL)
             kirk_path = os.path.join(self.PAYLOAD_PATH, kirk_filename)
             trust_path = os.path.join(self.PAYLOAD_PATH, truststore_filename)
+            sshd_payload_path = os.path.join(self.PAYLOAD_PATH, sshd_config_filename)
             syslog_conf_path = os.path.join(self._work_dir, syslog_conf_filename)
             elasticsearch_path = os.path.join(self._work_dir, elasticsearch_filename)
             install_path = os.path.join(self._work_dir, install_script_filename)
+
             
             with open(syslog_conf_path, 'w') as f:
                 f.write(self.SYSLOG_VIRTUE_NODE_CONF % (self._args.elastic_search_node, self._args.syslog_server))
@@ -170,7 +172,7 @@ searchguard.ssl.transport.enforce_hostname_verification: false
             self._copy_file(module_path, self.MODULE_TARBALL)
             self._copy_file(kirk_path, kirk_filename)
             self._copy_file(trust_path, truststore_filename)
-            self._copy_file(sshd_config_path, sshd_config_filename)
+            self._copy_file(sshd_payload_path, sshd_config_filename)
 
             self._exec_cmd('chmod +x %s' % (install_script_filename))
             self._exec_cmd('sudo ./%s' % (install_script_filename))
