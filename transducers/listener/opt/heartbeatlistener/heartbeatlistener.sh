0b49cbcf71538642cdc48ef8681856e2709ee168
Farhan Patwa <fpatwa@users.noreply.github.com>
2018-06-14 16:41:30 -0500

    Added else clause for sourcing virtual env.
    Source the virtual environment if virtual env is already created.

--- transducers/listener/opt/heartbeatlistener/heartbeatlistener.sh
+++ transducers/listener/opt/heartbeatlistener/heartbeatlistener.sh
@@ -7,7 +7,9 @@ if [ ! -d "venv" ]; then
   sudo apt install virtualenv
   virtualenv venv
   source ./venv/bin/activate
-  pip install -r ./requirements.txt
+  pip install -r ./requirements.txt  
+else
+  source ./venv/bin/activate
 fi
 # Now start the listener
 python /opt/heartbeatlistener/listener.py .excalibur
