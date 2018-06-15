465625dd292fe4618df0f6c2a6c020826c48185e
Farhan Patwa <farhan.patwa@starlab.io>
2018-06-15 13:32:13 -0500

    Added virtualenv which is needed excalibur server

--- tests/setup/setup_excalibur.sh
+++ tests/setup/setup_excalibur.sh
@@ -6,6 +6,6 @@ BASE_DIR="galahad/tests/setup"
 sudo apt-get update
 # Cannot yet automate responses to three-way merge prompts
 #sudo apt-get upgrade -y
-sudo apt-get install -y python-pip libldap2-dev libsasl2-dev python-logilab-common
+sudo apt-get install -y virtualenv python-pip libldap2-dev libsasl2-dev python-logilab-common
 sudo apt-get autoremove -y
 sudo pip install --upgrade -r $HOME/$BASE_DIR/requirements.txt
