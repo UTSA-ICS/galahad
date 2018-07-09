e962c7d12d066ff2b590fd7678df8cd4a4d17eb4
Ubuntu <ubuntu@ip-172-30-87-98.ec2.internal>
2018-07-09 19:41:35 +0000

    gaius with migration controls initial commit

---
+++ gaius/gaius/bin/cleanup.sh
@@ -0,0 +1,4 @@
+xl destroy $1
+rm $1.cfg
+rm /mnt/nfs/vms/images/$1.img
+
---
+++ gaius/gaius/run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#begins the changefeed
+from gaius.changefeed import changes
+changes = changes()
+changes.main()
