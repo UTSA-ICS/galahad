8bfffa014882a5e11ea3ed68fd4bee20ffcf6161
zratliff <zachary.b.ratliff@raytheon.com>
2018-07-10 10:01:58 -0400

    Updated setup file and Makefile to run on a virtue

--- transducers/actuators/networkactuator/setup.sh
+++ transducers/actuators/networkactuator/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+headers="linux-headers-$(uname -r)"
+yes | sudo apt-get install $headers
 make
 sudo insmod actuator_network.ko
 sudo cp netblock /usr/bin
@@ -8,4 +10,3 @@ yes | sudo apt-get install sed
 sudo touch "/tmp/netblock.dat"
 sudo chmod +x netblock
 echo "Setup complete. Run sudo netblock to add firewall rules"
-
