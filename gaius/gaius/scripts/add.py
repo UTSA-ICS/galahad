31a3b65006e0b18712ac982cfd45d5aedc61c530
root <root@ip-172-30-87-99.ec2.internal>
2018-07-11 14:40:46 +0000

    only adds, removes, and migrates virtues on the correct valor

--- gaius/gaius/scripts/add.py
+++ gaius/gaius/scripts/add.py
@@ -5,5 +5,5 @@ r.connect("172.30.93.138",28015).repl()
 r.db('routing').table('galahad').insert([{
 	'function':'virtue',
 	'host':'centos-test',
-	'address':'172.30.87.98',
+	'address':'172.30.87.99',
 	'guestnet':'10.91.0.5'}]).run()
