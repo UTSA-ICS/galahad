ebb996ce5416c1d419f48aa82ee497a66046e748
Ubuntu <ubuntu@ip-172-30-87-100.ec2.internal>
2018-07-18 13:40:11 +0000

    name cleanup

--- gaius/gaius/bin/migrate.sh
+++ gaius/gaius/bin/migrate.sh
@@ -2,4 +2,3 @@ ssh-keyscan -H $2 >> /root/.ssh/known_hosts
 xl migrate $1 $2
 ssh-keygen -R $2
 rm /root/.ssh/known_hosts.old
-
--- gaius/gaius/scripts/transducer.py
+++ gaius/gaius/scripts/transducer.py
@@ -6,13 +6,3 @@ r.db('routing').table('transducer').filter(r.row['transducerId']=='m1').update({
 r.db('routing').table('transducer').filter(r.row['transducerId']=='m1').run()
 r.db('routing').table('transducer').filter(r.row['transducerId']=='m1').update({'flag':'FALSE'}).run()
 r.db('routing').table('transducer').filter(r.row['transducerId']=='m1').run()
-
-#config = {'host':'nx.tx','newHost':'TestNode.x'}
-#r.db("routing").table("transducer").insert({
-#    "transducerId" : "m1",
-#    "flag" : "FALSE",
-#    "config": config,
-#    "history" : []
-#}).run()
-
-#r.db("routing").table("transducer").filter(r.row["transducerId"]=="test").delete().run()
