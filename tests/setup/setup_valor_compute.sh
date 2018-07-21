2cb75b7ac5477556e8221d5a94e0c73a78a077f0
Farhan Patwa <farhan.patwa@starlab.io>
2018-07-20 20:26:45 -0500

    Commented out reboot directive as base image has some issues
    The reboot directive needs to be added back in after the base image
    issue is resolved.
    Due to this issue once a reboot is issued on a XenBlanket-Clean based
    instance a kernel panic is encountered and the instance gets stuck.

--- tests/setup/setup_valor_compute.sh
+++ tests/setup/setup_valor_compute.sh
@@ -6,4 +6,4 @@ sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retr
 sudo cp -R /mnt/nfs/deploy/compute/config /home/ubuntu
 cd /home/ubuntu/config
 sudo /bin/bash setup.sh
-sudo reboot
+#sudo reboot
