c3bbbf36ddc6d9f9762b66528f919d757342cccf
Farhan Patwa <farhan.patwa@starlab.io>
2018-07-31 08:23:12 -0500

    Fixes for correct syntax and copying over files to the right directory

--- tests/setup.py
+++ tests/setup.py
@@ -243,16 +243,16 @@ class Excalibur():
         GALAHAD_CONFIG_DIR = '~/galahad-config'
         _cmd5 = "cp('{0}/excalibur_pub.pem {1}/excalibur_pub.pem')".format(GALAHAD_CONFIG_DIR, GALAHAD_KEY_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd5)
-        _cmd5 = "cp('{0}/rethinkdb_keys/rethinkdb_cert.pem {1}/rethinkdb_cert.pem')".format(GALAHAD_CONFIG_DIR, GALAHAD_KEY_DIR)
+        _cmd5 = "cp('{0}/rethinkdb_keys/rethinkdb_cert.pem {1}/')".format(GALAHAD_CONFIG_DIR, GALAHAD_KEY_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd5)
 
         # Now populate the /var/private/ssl dir for excalibur
         EXCALIBUR_PRIVATE_DIR = '/var/private/ssl'
-        _cmd6 = "mkdir('-p {}')".format(EXCALIBUR_PRIVATE_DIR)
+        _cmd6 = "sudo('mkdir -p {0}').and_().sudo('chown -R ubuntu.ubuntu /var/private')".format(EXCALIBUR_PRIVATE_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd6)
-        _cmd6 = "cp('{0}/excalibur_private_key.pem.pem {1}/')".format(GALAHAD_CONFIG_DIR, EXCALIBUR_PRIVATE_DIR)
+        _cmd6 = "cp('{0}/excalibur_private_key.pem {1}/')".format(GALAHAD_CONFIG_DIR, EXCALIBUR_PRIVATE_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd6)
-        _cmd6 = "cp('{0}/rethinkdb_cert.pem {1}/')".format(GALAHAD_CONFIG_DIR, EXCALIBUR_PRIVATE_DIR)
+        _cmd6 = "cp('{0}/rethinkdb_keys/rethinkdb_cert.pem {1}/')".format(GALAHAD_CONFIG_DIR, EXCALIBUR_PRIVATE_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd6)
         _cmd6 = "cp('-r {0}/elasticsearch_keys {1}/')".format(GALAHAD_CONFIG_DIR, EXCALIBUR_PRIVATE_DIR)
         run_ssh_cmd(self.server_ip, self.ssh_key, _cmd6)
