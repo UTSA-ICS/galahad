dcc4bdc99f7d59671a2b5e0c593bdd23e1662ab4
Maria <mariya.zakhalyavko@raytheon.com>
2018-08-01 15:36:52 -0400

    Fix merlin systemd service file
    Service file now both starts and stops correctly.
    
    Also new transducer-module.tar.gz because the previous one was corrupted.

--- assembler/stages/merlin.py
+++ assembler/stages/merlin.py
@@ -12,35 +12,16 @@ class MerlinStage(SSHStage):
     PAYLOAD_PATH = 'payload'
     DEB_FILE = 'merlin.deb'
 
-    SYSTEMCTL_UNIT = '''[Unit]
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/etc/init.d/merlin start
-
-[Install]
-WantedBy=multi-user.target
-'''
-
     def run(self):
         if not self._has_run:
             super().run()
             deb_file_path = os.path.join(self.PAYLOAD_PATH, self.DEB_FILE)
-            systemctl_vmpath = '/etc/systemd/system/merlin.service'
-            systemctl_filename = os.path.basename(systemctl_vmpath)
-            systemctl_path = os.path.join(self._work_dir, systemctl_filename)
-
-            with open(systemctl_path, 'w') as f:
-                f.write(self.SYSTEMCTL_UNIT)
-            self._copy_file(systemctl_path, systemctl_filename)
             self._copy_file(deb_file_path, self.DEB_FILE)
 
             self._exec_cmd('sudo dpkg -i %s' % (self.DEB_FILE))
             self._exec_cmd('sudo chown -R merlin:camelot /opt/merlin')
             self._exec_cmd('sudo chmod 777 /opt/merlin')
             self._exec_cmd('sudo chown -R merlin:camelot /var/private/ssl')
-            self._exec_cmd('sudo mv %s %s' % (systemctl_filename, systemctl_vmpath))
             self._exec_cmd('sudo systemctl enable merlin')
             self._exec_cmd('sudo systemctl start merlin')
             self._exec_cmd('sudo rm merlin.deb')
