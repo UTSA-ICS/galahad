b1466f3a08f28318dc14506538c81e5d85264a7d
Farhan Patwa <farhan.patwa@starlab.io>
2018-08-20 12:15:17 -0500

    This script will handle setup of EFS on excalibur node

--- tests/setup/setup_efs.sh
+++ tests/setup/setup_efs.sh
@@ -2,13 +2,12 @@
 
 # Install and configure EFS file server on EFS server node
 EFS_ID="${1}"
-NFS_IP="${2}"
+
+# Base directory for Valor related files
+BASE_DIR="galahad/valor"
 
 sudo apt-get update
 sudo apt install --assume-yes nfs-common
 sudo mkdir /mnt/efs
 sudo mount -t nfs $EFS_ID:/ /mnt/efs
-sudo mkdir /mnt/nfs
-sudo mount -t nfs $NFS_IP:/ /mnt/nfs
-sudo cp -R /mnt/nfs/export/deploy /mnt/efs
-sudo cp /mnt/nfs/export/vms/images/centos7.img /mnt/efs
+sudo cp -R $HOME/$BASE_DIR/deploy /mnt/efs
