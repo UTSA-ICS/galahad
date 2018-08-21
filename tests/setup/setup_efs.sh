76e123ec450ff0356c6af78f3c9cd9d862d635d2
Farhan Patwa <farhan.patwa@starlab.io>
2018-08-21 07:36:17 -0500

    Added missing mount command (to mount from fstab)

--- tests/setup/setup_efs.sh
+++ tests/setup/setup_efs.sh
@@ -10,4 +10,5 @@ sudo apt-get update
 sudo apt install --assume-yes nfs-common
 sudo mkdir -p /mnt/efs
 sudo su - root -c "echo \"${EFS_ID}:/ /mnt/efs nfs defaults 0 0\" >> /etc/fstab"
+sudo mount -a
 sudo cp -R $HOME/$BASE_DIR/deploy /mnt/efs
--- tests/setup/setup_valor_compute.sh
+++ tests/setup/setup_valor_compute.sh
@@ -4,6 +4,7 @@ EFS_ID="${1}"
 
 sudo mkdir -p /mnt/efs
 sudo su - root -c "echo \"${EFS_ID}:/ /mnt/efs nfs defaults 0 0\" >> /etc/fstab"
+sudo mount -a
 sudo cp -R /mnt/efs/deploy/compute/config /home/ubuntu
 cd /home/ubuntu/config
 sudo /bin/bash setup.sh
