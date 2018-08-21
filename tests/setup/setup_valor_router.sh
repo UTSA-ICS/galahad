b1e9bc99bc015c94e879a45a378c07abaa54baee
Kelli Little <kellilittle@NotShanes-1861.hsv.starlab.io>
2018-08-21 09:41:43 -0500

    rc-local works after first reboot

--- tests/setup/setup_valor_router.sh
+++ tests/setup/setup_valor_router.sh
@@ -6,4 +6,5 @@ sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retr
 sudo cp -R /mnt/nfs/deploy/router/config /home/ubuntu
 cd /home/ubuntu/config
 sudo /bin/bash setup.sh
+sudo systemctl restart rc-local
 sudo shutdown -r
