ea6c38872554b0e0186453bcd36655340f8a7752
Mark V Senofsky <mark.senofsky@starlab.io>
2018-09-18 10:10:47 -0500

    I reorganized how RethinkDB is set up

---
+++ tests/setup/setup_rethinkdb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Retrieve keys and put them in their appropriate location
+sudo mkdir -p /var/private/ssl/
+git clone git@github.com:starlab-io/galahad-config.git
+
+sudo cp galahad-config/rethinkdb_keys/rethinkdb.pem      /var/private/ssl/
+sudo cp galahad-config/rethinkdb_keys/rethinkdb_cert.pem /var/private/ssl/
+sudo cp galahad-config/excalibur_private_key.pem         /var/private/ssl/excalibur_key.pem
+
+sudo chown rethinkdb:rethinkdb /var/private/ssl/*.pem
+sudo chmod 600 /var/private/ssl/*.pem
+
+
+# Set the hostname
+echo "127.0.0.1 rethinkdb.galahad.lab" >> /etc/hosts
+
+
+# Add RethinkDB repository and install
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update
+apt-get --assume-yes install rethinkdb
+
+
+# Put RethinkDB configuration file in its appropriate location
+sudo cp rethinkdb.conf /etc/rethinkdb/instances.d/
+
+
+# Restart RethinkDB with its new configuration file
+sudo service rethinkdb restart
+
+
+# Install python libraries for rethinkdb and configure the database
+apt-get --assume-yes install python2.7 python-pip
+pip install rethinkdb
+sudo python configure_rethinkdb.py
