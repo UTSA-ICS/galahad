c6daaa7ba6f6938cad167d551f7a1510ff84d757
Farhan Patwa <farhan.patwa@starlab.io>
2018-10-01 13:55:08 -0500

    Copy over the galahad-keys dir to EFS mount

--- tests/setup/setup_efs.sh
+++ tests/setup/setup_efs.sh
@@ -11,8 +11,10 @@ sudo apt install --assume-yes nfs-common
 sudo mkdir -p /mnt/efs
 sudo su - root -c "echo \"${EFS_ID}:/ /mnt/efs nfs defaults 0 0\" >> /etc/fstab"
 sudo mount -a
-sudo cp -R $HOME/$BASE_DIR/deploy /mnt/efs
+sudo cp -Rp $HOME/$BASE_DIR/deploy /mnt/efs
 sudo mkdir -p /mnt/efs/images/base_ubuntu
 sudo mkdir -p /mnt/efs/images/unities
 sudo mkdir -p /mnt/efs/images/non_p_virtues
 sudo mkdir -p /mnt/efs/images/p_virtues
+# Copy over the galahad-keys to EFS mount
+sudo cp -Rp $HOME/galahad-keys /mnt/efs
