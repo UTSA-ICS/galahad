a8b6bf3d13108de6dc1aeb5596230ff3784dd3b5
Farhan Patwa <farhan.patwa@starlab.io>
2018-10-12 14:12:35 -0500

    Changed name of port to include last 2 tuples of IP address

--- valor/deploy/router/add_valor.sh
+++ valor/deploy/router/add_valor.sh
@@ -6,8 +6,7 @@ VALOR_IP="${1}"
 # Add a port in the ovs switch with Valor's remote IP
 #
 while read line; do
-        echo $line
 	IFS='.' read -r -a array <<< "$line"
-	port="vxlan"${array[3]}
+	port="vxlan"${array[2]}${array[3]}
 	ovs-vsctl add-port hello-br0 $port -- set interface $port type=vxlan options:remote_ip=$line
 done <<< $VALOR_IP
