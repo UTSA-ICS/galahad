4f9da783b80cbafc77a15e4cef88b87156108708
Farhan Patwa <farhan.patwa@starlab.io>
2018-10-17 18:05:58 -0500

    Changed to run command as sudo

--- tests/setup/setup_excalibur.sh
+++ tests/setup/setup_excalibur.sh
@@ -6,10 +6,10 @@ BASE_DIR="galahad/tests/setup"
 #
 # Setup Routes to be able to get to the guestnet network for access to virtues
 #
-echo "  #" >> /etc/network/interfaces.d/50-cloud-init.cfg
-echo "  # Routes to be able to reach the virtue guestnet subnet" >> /etc/network/interfaces.d/50-cloud-init.cfg
-echo "  #" >> /etc/network/interfaces.d/50-cloud-init.cfg
-echo "  post-up route add -net 10.91.0.0/16 gw 172.30.1.53" >> /etc/network/interfaces.d/50-cloud-init.cfg
+sudo su - root -c "echo \"  #\"                                                       >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  # Routes to be able to reach the virtue guestnet subnet\" >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  #\"                                                       >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  post-up route add -net 10.91.0.0/16 gw 172.30.1.53\"      >> /etc/network/interfaces.d/50-cloud-init.cfg"
 
 sudo apt-get update
 # Cannot yet automate responses to three-way merge prompts
