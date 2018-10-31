be6da5a946b90ada9a9cd68ddcd4c35152addf55
Farhan Patwa <farhan.patwa@starlab.io>
2018-10-31 11:44:21 -0500

    Changed the default MTU for eth0 to 1500 (from 9000)
    - The default MTU for the ec2 instances is 9000. This is to support
    Jumbo Frames. The issue is that when communicating with the virtue
    network (guestnet subnet) the OVS devices/interfaces all have a default
    MTU of 1500. This causes the SSH connection from excalibur node to the
    guestnet subnet (10.91.x.x) network to hang on high network traffic.
    Changed the MTU on excalibur to 1500 and this resolves the network
    issue.

--- deploy/setup/setup_excalibur.sh
+++ deploy/setup/setup_excalibur.sh
@@ -6,13 +6,22 @@ BASE_DIR="galahad/deploy/setup"
 #
 # Setup Routes to be able to get to the guestnet network for access to virtues
 #
-sudo su - root -c "echo \"  #\"                                                       >> /etc/network/interfaces.d/50-cloud-init.cfg"
-sudo su - root -c "echo \"  # Routes to be able to reach the virtue guestnet subnet\" >> /etc/network/interfaces.d/50-cloud-init.cfg"
-sudo su - root -c "echo \"  #\"                                                       >> /etc/network/interfaces.d/50-cloud-init.cfg"
-sudo su - root -c "echo \"  post-up route add -net 10.91.0.0/16 gw 172.30.1.53\"      >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  #\"                                                                        >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  # Routes to be able to reach the virtue guestnet subnet (virtue network)\" >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  #\"                                                                        >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  post-up route add -net 10.91.0.0/16 gw 172.30.1.53\"                       >> /etc/network/interfaces.d/50-cloud-init.cfg"
 # Added the routes temporarily so to take affect without a reboot
 sudo route add -net 10.91.0.0/16 gw 172.30.1.53 dev eth0
 
+#
+# Change the default MTU to match with what is configured for the guestnet OVS interfaces (i.e 1500 MTU)
+#
+sudo su - root -c "echo \"  #\"                                                                                   >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  # Change MTU to 1500 to be compatible with guestnet subnet devices (virtue network)\" >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  #\"                                                                                   >> /etc/network/interfaces.d/50-cloud-init.cfg"
+sudo su - root -c "echo \"  mtu 1500\"                                                                            >> /etc/network/interfaces.d/50-cloud-init.cfg"
+# Change the MTU temporarily so as to take affect without a reboot
+ip link set dev eth0 mtu 1500
 
 sudo apt-get update
 # Cannot yet automate responses to three-way merge prompts
