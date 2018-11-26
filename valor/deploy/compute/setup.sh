b0bd10ef6ad499eec01734e3b19cda09b6fd266c
Farhan Patwa <farhan.patwa@starlab.io>
2018-11-26 14:29:05 -0600

    Move only the apt package and EFS mount to cloud-init
    - Revert some of the previous changes for code clarity

--- valor/deploy/compute/setup.sh
+++ valor/deploy/compute/setup.sh
@@ -8,30 +8,29 @@ ROUTER_IP="${2}"
 GALAHAD_KEY_DIR="/mnt/efs/galahad-keys"
 
 #
-# Create a openvswitch bridge - hello-br0
+# Disable cloud init networking which sets eth0 to default settings.
 #
-ovs-vsctl add-br hello-br0
+echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+rm -f /etc/network/interfaces.d/50-cloud-init.cfg
 
 #
-# Configure bridge hello-br0
+# Setup and configure bridge br0 with interface eth0
 #
 echo "" >> /etc/network/interfaces
 echo "#" >> /etc/network/interfaces
-echo "# Bridge hello-br0 for ovs bridge hello-br0" >> /etc/network/interfaces
+echo "# Bridge br0 for eth0" >> /etc/network/interfaces
 echo "#" >> /etc/network/interfaces
-echo "auto hello-br0" >> /etc/network/interfaces
-echo "iface hello-br0 inet static" >> /etc/network/interfaces
-echo "  address ${GUESTNET_IP}/24" >> /etc/network/interfaces
+echo "auto br0" >> /etc/network/interfaces
+echo "iface br0 inet dhcp" >> /etc/network/interfaces
+echo "  bridge_ports br0 eth0" >> /etc/network/interfaces
+echo "  bridge_stp off" >> /etc/network/interfaces
+echo "  bridge_fd 0" >> /etc/network/interfaces
+echo "  bridge_maxwait 0" >> /etc/network/interfaces
 
 #
-# Set the Network System Variables
+# Call the base setup script
 #
-sed -i 's/#net.ipv4.ip_forward/net.ipv4.ip_forward/' /etc/sysctl.conf
-echo "#" >> /etc/sysctl.conf
-echo "# Network variables for Valor Network" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.rp_filter=0" >> /etc/sysctl.conf
-echo "net.ipv4.conf.gre0.rp_filter=0" >> /etc/sysctl.conf
-
+/bin/bash ../base_setup.sh "${GUESTNET_IP}"
 
 #
 # Install necessary System packages
