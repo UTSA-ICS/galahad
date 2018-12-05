f87d6b12ae6dcad416bfbadde362879132c36c1c
Farhan Patwa <farhan.patwa@starlab.io>
2018-12-05 13:10:20 -0600

    Fixed invalid directory names

--- deploy/setup/setup_canvas.sh
+++ deploy/setup/setup_canvas.sh
@@ -9,10 +9,8 @@ GALAHAD_KEY_DIR="/mnt/efs/galahad-keys"
 #
 # Copy over the private key to use to login to the virtue
 #
-cp $GALAHAD_KEY_DIR/default-virtue-key.pem $CANVAS_DIR/key.pem
+cp $GALAHAD_KEY_DIR/default-virtue-key.pem $HOME/$CANVAS_DIR/key.pem
 
-cd $CANVAS_DIR
+cd $HOME/$CANVAS_DIR
 echo "$HOME/nwjs-sdk-*/nw --enable-transparent-visuals --disable-gpu --disable-gpu-compositing --force-cpu-draw ." > start_canvas.sh
 chmod +x start_canvas.sh
-
-
