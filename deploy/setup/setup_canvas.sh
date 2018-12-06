a19c8b374b1ecb7f8375bbed86798260f8d1ec45
Farhan Patwa <farhan.patwa@starlab.io>
2018-12-05 23:43:30 -0600

    Added npm package install and module install
    - Added DNS map to canvas.com (127.0.0.1)
    - Installed npm package
    - Installed npm modules in canvas directry

--- deploy/setup/setup_canvas.sh
+++ deploy/setup/setup_canvas.sh
@@ -12,5 +12,6 @@ GALAHAD_KEY_DIR="/mnt/efs/galahad-keys"
 cp $GALAHAD_KEY_DIR/default-virtue-key.pem $HOME/$CANVAS_DIR/key.pem
 
 cd $HOME/$CANVAS_DIR
+npm install
 echo "$HOME/nwjs-sdk-*/nw --enable-transparent-visuals --disable-gpu --disable-gpu-compositing --force-cpu-draw ." > start_canvas.sh
 chmod +x start_canvas.sh
