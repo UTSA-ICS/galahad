5d5a7c96c8bfb6ae0983a57e8f50903f838f8632
Farhan Patwa <farhan.patwa@starlab.io>
2018-12-06 23:58:08 -0600

    Updated to update the config.json with correct OAUTH Client ID
    - copied sso_login from cli
    - created method to get the OAUTH client ID and then update config.json
    with it
    - updated setup to update config.json with latest value

--- deploy/setup/setup_canvas.sh
+++ deploy/setup/setup_canvas.sh
@@ -11,6 +11,12 @@ GALAHAD_KEY_DIR="/mnt/efs/galahad-keys"
 #
 cp $GALAHAD_KEY_DIR/default-virtue-key.pem $HOME/$CANVAS_DIR/key.pem
 
+#
+# Update config.json file
+#
+cd $HOME/$CANVAS_DIR
+python3 update_config_json.py -u 'jmitchell@virtue.com' -p 'Test123!'
+
 cd $HOME/$CANVAS_DIR
 npm install
 echo "$HOME/nwjs-sdk-*/nw --enable-transparent-visuals --disable-gpu --disable-gpu-compositing --force-cpu-draw ." > start_canvas.sh
