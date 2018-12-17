13bea8a72d1312032865f7f25ecc3d7bb1bac656
Farhan Patwa <farhan.patwa@starlab.io>
2018-12-17 12:25:56 -0600

    Added options in role_create for passing in Application name and role name

--- demo/role_create.sh
+++ demo/role_create.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $# != 2 ]];
+if [[ $# != 3 ]];
 then
   echo ""
-  echo "Enter Application name and unity size e.g"
-  echo "  $0 firefox 4GB"
+  echo "Enter Role Name, Application name and unity size e.g"
+  echo "  $0 Internet-Browsing firefox 4GB"
+  echo ""
+  echo "If there is more than 1 application then list the names comma separated and in quotes preceeded with backslash (\) e.g:"
+  echo "  $0 Banking \"firefox\\\",\\\"wincmd\" 8GB"
   echo ""
   exit 0
 fi
@@ -19,9 +22,11 @@ export VIRTUE_TOKEN=$(cat usertoken.json)
 echo -e '{
     "name": '\"$1\"',
     "version": "1.0",
-    "applicationIds": ['\"$1\"'],
+    "applicationIds": ['\"$2\"'],
     "startingResourceIds": [],
-    "startingTransducerIds": []
+    "startingTransducerIds": ["path_mkdir", "bprm_set_creds", "task_create", "task_alloc", "inode_create", "socket_connect", "socket_bind", "socket_accept", "socket_listen", "create_process", "process_start", "process_died", "srv_create_proc", "open_fd"]
 }' > role.json
 
 python3 virtue-admin role create --role=role.json --unitySize=$2
+
+cat role.json
