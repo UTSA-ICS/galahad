7014d31d308eed54a81707722c7e01d5580e2dbd
Kelli Little <kellilittle@NotShanes-1861.hsv.starlab.io>
2019-02-06 10:32:50 -0600

    add AD NETBIOS name resolution

--- deploy/setup/setup_excalibur.sh
+++ deploy/setup/setup_excalibur.sh
@@ -17,8 +17,11 @@ sudo apt-get update
 # Cannot yet automate responses to three-way merge prompts
 #sudo apt-get upgrade -y
 sudo apt-get install -y virtualenv python-pip libldap2-dev libsasl2-dev python-logilab-common nfs-common
-sudo DEBIAN_FRONTEND=noninteractive apt-get install krb5-user
+sudo DEBIAN_FRONTEND=noninteractive apt-get install krb5-user -y
 sudo apt-get autoremove -y
 sudo pip install --upgrade -r $HOME/$BASE_DIR/requirements.txt
+
+# kerberos setup
 sudo rm /etc/krb5.conf
 sudo cp $HOME/$BASE_DIR/krb5.conf /etc
+sudo echo "172.30.1.250 camelot.virtue.gov camelot >> /etc/hosts"
