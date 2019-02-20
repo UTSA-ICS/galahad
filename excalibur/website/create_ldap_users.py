bcc9589a50bd8e83a0cd075e8ab9a869452f32a7
Kelli Little <kelli.little@starlab.io>
2019-02-20 15:11:07 +0000

    fix login with username instead of name

--- excalibur/website/create_ldap_users.py
+++ excalibur/website/create_ldap_users.py
@@ -3,7 +3,6 @@
 import ldap_tools
 from ldaplookup import LDAP
 
-
 def get_ldap_usernames(ldap):
     # Query and get all the usernames from open ldap server
     ldap_users = ldap_tools.parse_ldap_list(
@@ -36,9 +35,12 @@ def update_ldap_users_from_ad():
 
     # Go through all users in AD
     for user in ad_users:
-        ad_username = user[1]['cn'][0]
+        cn = user[1]['cn'][0]
+        ad_username = user[1]['sAMAccountName'][0]
         # If a username in AD is not found in ldap then add it
         if ad_username not in ldap_usernames:
-            new_user = {'username': ad_username, 'authorizedRoleIds': []}
+            new_user = {'username': ad_username, 'authorizedRoleIds': [], 'name': cn}
             new_ldap_user = ldap_tools.to_ldap(new_user, 'OpenLDAPuser')
             ldap.add_obj(new_ldap_user, 'users', 'cusername', throw_error=True)
+
+    return ldap
